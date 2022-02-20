dfca20bf06b49a0fe693056c9be399b5b7a19ad9
JesusArtz <realjesusartz@gmail.com>
2022-02-19 21:16:59 -0600

    Added a practice codes

---
+++ fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n=0;
+    int temp=0;
+
+    cin>>n;
+
+    while (true){
+        cout<<n<<" ";
+        if(n!=0){
+            temp = n;
+            n = n+temp;
+            cout<<n<<" ";
+        }if (n>45){
+            break;
+        }
+        
+    }
+    
+
+}
---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void print(char a){
+
+    std::cout<<a;
+
+}
+
+int main(){
+    const b="Hello world!";
+
+    print(b);
+
+}
